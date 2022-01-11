e3099ec860915a65005949a04e5bb85750c49331
David Zuñiga <davidzunigaortiz92@gmail.com>
2022-01-11 12:23:54 -0500

    Google Sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
 
@@ -52,6 +53,60 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token} = req.body;
+
+    try {
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if( !usuario){
+            const data = {
+                nombre,
+                correo,
+                password:':P',
+                img,
+                google:true,
+                rol:'ADMIN_ROLE'
+
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en DB niego la autenticación
+        if( !usuario.estado) {
+            return res.status(401).json({
+                msg:'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //generqar el jwt
+        const token = await generarJWT(usuario.id);
+        
+        
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(400).json({
+            msg:'El token no se puso verificar',
+            ok: false
+        })
+        
+    }
+    
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
